33e4864e4be0b1d9bf2de74c9d03a4eedb86bc78
nicypicy <varshachowdary040@gmail.com>
2024-09-30 17:08:32 +0530

    more to come

---
+++ src/app/email-verify/email-verify.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-verify',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-verify.component.html',
+  styleUrl: './email-verify.component.css'
+})
+export class EmailVerifyComponent {
+
+}
